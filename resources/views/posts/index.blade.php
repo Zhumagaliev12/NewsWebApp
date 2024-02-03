a9e10193d6b442f7995893ed6a553f20d38edeac
me1r <zhmglv12@gmail.com>
2024-02-03 18:43:27 +0600

    Add several routes and views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,17 @@
     <title>Main page</title>
 </head>
 <body>
-    <a href=" {{ route('posts.create') }} ">Go to create page</a>
+    <div>
+        <nav>
+            <li>
+                <ul style="display: inline"><a href=" {{ route('main.index') }} ">Main</a></ul>
+                <ul style="display: inline"><a href=" {{ route('main.about') }} ">About Us</a></ul>
+                <ul style="display: inline"><a href=" {{ route('main.contact') }} ">Contact</a></ul>
+                <ul style="display: inline"><a href=" {{ route('posts.create') }} ">Create Post</a></ul>
+            </li>
+        </nav>
+    </div>
+{{--    <a href=" {{ route('posts.create') }} ">Go to create page</a>--}}
 
     @foreach($posts as $post)
         <a href="{{route('posts.show', $post->id)}}"><h3>{{$post->title}}</h3></a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\MyClassController;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return Route::redirect('posts.index');
@@ -10,3 +11,6 @@ Route::get('/', function () {
 
 Route::resource('posts', MyClassController::class);
 
+Route::get('main', 'App\Http\Controllers\MainController@index')->name('main.index');
+Route::get('about', 'App\Http\Controllers\AboutController@index')->name('main.about');
+Route::get('contact', 'App\Http\Controllers\ContactController@index')->name('main.contact');
