14fd38b1a9588506d39bbdd8006fd4ef02e00c15
me1r <zhmglv12@gmail.com>
2024-02-07 01:38:12 +0600

    Add some styles

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,14 +6,16 @@
     <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </head>
 <body>
-    <a href=" {{ route('posts.index') }} ">Go to index page</a>   <br><br>
+    <div class="container">
+    <a href=" {{ route('posts.index') }} " class="btn btn-info" style="text-decoration: none;">Go to index page</a>   <br><br>
 
     <form action="{{ route('posts.store') }}" method="post">
         @csrf
-        Title: <input type="text" name="title"><br><br>
-        Content: <textarea name="content" cols="30" rows="10"></textarea><br><br>
+        <input type="text" name="title" placeholder="Title"><br><br>
+        <textarea name="content" cols="30" rows="10" placeholder="Content"></textarea><br><br>
         <input type="hidden" value="1" name="is_published">
-        <button type="submit">Create post</button>
+        <button type="submit" class="btn btn-success">Create post</button>
     </form>
+    </div>
 </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,18 +7,24 @@
     <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </head>
 <body>
-    <a href=" {{ route('posts.index') }} " class="btn btn-primary m-3 ">Go to index page</a>
 
-    <h3>{{$post->title}}</h3>
-    <p>{{$post->content}}</p>
 
-    <a  href="{{route('posts.edit', $post->id)}} " class="btn btn-info m-2">Edit</a>
+    <div class="container">
+        <a href=" {{ route('posts.index') }} " class="btn btn-primary mt-2">Go to index page</a>
+        <div class="mt-2">
+        <h3>{{$post->title}}</h3>
+        <p>{{$post->content}}</p>
+        </div>
+        <a  href="{{route('posts.edit', $post->id)}} " class="btn btn-info mt-2">Edit</a>
+        <form action="{{route('posts.destroy',$post->id)}}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger mt-2">Delete</button>
+        </form>
+    </div>
 
-    <form action="{{route('posts.destroy',$post->id)}}" method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger m-2">Delete</button>
-    </form>
+
+
 
 </body>
 </html>
