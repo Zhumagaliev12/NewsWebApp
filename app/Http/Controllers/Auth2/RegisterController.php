21aefaabec4c4803ba30178742208decd3da2a80
me1r <zhmglv12@gmail.com>
2024-02-16 03:06:49 +0600

    Registration and authorization were added, and the style of the site was changed.

---
+++ app/Http/Controllers/Auth2/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth2;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+           'name' => 'required|max:255|',
+           'email' => 'required|email|max:255|unique:users',
+           'password' => 'required|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('posts.index');
+    }
+}
